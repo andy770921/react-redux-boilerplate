4edc779e7d0379b0e2db5235677a3f722c5a98f1
Andy Chou <andy770921@yahoo.com.tw>
2020-03-29 00:30:30 +0800

    feat: add react-router basename

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ render(
     <>
         <GlobalStyle />
         <Provider store={store}>
-            <Router>
+            <Router basename="/dist">
                 <App />
             </Router>
         </Provider>
