37d699d3898817ef498fc8f25c57c2ddf2a302ae
Andy Chou <andy770921@yahoo.com.tw>
2020-04-05 03:34:28 +0800

    test: test process.env.PUBLIC_URL

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ render(
     <>
         <GlobalStyle />
         <Provider store={store}>
+            {console.log('process.env.PUBLIC_URL', process.env.PUBLIC_URL)}
             <Router basename={process.env.PUBLIC_URL}>
                 <App />
             </Router>
